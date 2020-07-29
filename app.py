948d5bc7a33b6e39301826aa41d2c2c81af75317
muhaiminsarker <sarker.muhaimin15@gmail.com>
2020-07-29 16:28:15 +0000

    about and donate pages

--- app.py
+++ app.py
@@ -12,7 +12,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
+@app.route("/donate")
+def donate():
+    return render_template("donate.html")
+    
 @app.route('/australia')
 
 def australia():
